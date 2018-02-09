172a612c3c81bc5373fbe77226aa47b4b17bbc75
peiyusen <yusenpei@163.com>
2018-02-09 15:06:56 +0800

    files read write operation

--- hello world.py
+++ hello world.py
@@ -12,19 +12,27 @@ print("你的分数是：%d" % score)
 # passwd=input("请输入密码:")
 # print("您的密码是%s"%passwd)
 
-name1 = input("请输入您的姓名：")
-QQ1 = input("请输入您的QQ号码：")
-phone1 = input("请输入您的手机：")
-addr1 = input("请输入您的公司地址：")
-
-print("系统正在生成您的名片，请稍候")
-print("==========================",
-      "姓名：%s" % name1,
-      "Q Q：%s" % QQ1,
-      "手机：%s" % phone1,
-      "地址：%s" % addr1,
-      "==========================", sep="\n")
+# name1 = input("请输入您的姓名：")
+# QQ1 = input("请输入您的QQ号码：")
+# phone1 = input("请输入您的手机：")
+# addr1 = input("请输入您的公司地址：")
+#
+# print("系统正在生成您的名片，请稍候")
+# print("==========================",
+#       "姓名：%s" % name1,
+#       "Q Q：%s" % QQ1,
+#       "手机：%s" % phone1,
+#       "地址：%s" % addr1,
+#       "==========================", sep="\n")
 
 files = open('D:\my_python_trial\myfile.txt', "w")  # "w"
 files.write("check this out")
 files.close()
+
+with open('D:\my_python_trial\myfile.txt', 'r') as f:
+    files_context = f.read()
+
+files1 = open('D:\my_python_trial\myfile1.txt', "w")  # "w"
+files1.write("data from the other file = %s"%files_context)
+files1.close()
+
