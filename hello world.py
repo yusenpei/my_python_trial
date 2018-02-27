8d07f5d147964ea225b7cbbf8af4d2aa93329cfc
宇森 <yusenpei@163.com>
2018-02-27 22:21:26 +0800

    some try at home

--- hello world.py
+++ hello world.py
@@ -1,9 +1,9 @@
 # D:\python
 # encoding=utf-8
 
-import time
+
 import re
-import sys
+
 
 # print("hello world")
 # print("hello worldp")
@@ -46,59 +46,56 @@ import sys
 # b=100
 # b += 1
 # print("%d"%b)
+
+#定义关键词
 key_word_entity = 'entity'
 key_word_end_entity = 'end entity'
 key_word_generic = 'generic'
-header =("="*15+"\n2018 new year"+"\nCOPYRIGHT@SNAPS\n"+"="*15+'\n')
-files = open('D:\my_python_trial\myfile.txt', "r")  # "r"
-all_content = files.read()
-entity_header_pattern  = re.compile(key_word_entity+'(.*?)'+key_word_end_entity,re.S)
-entity_arrow_pattern   = re.compile('=>'+'(.*?)'+';',re.S)
-# entity_header = entity_header_pattern.findall(all_content,text)
-# print("entity_header")
-files1 = open('D:\my_python_trial\myfile1.txt', "w")  # "w"
-instance_name = input('instance name?')
-files1.write(header +instance_name+':'+key_word_entity+'\n'.join(entity_header_pattern.findall(all_content)) + key_word_end_entity)
-files1.close()
+header = ("=" * 15 + "\n2018 new year" + "\nCOPYRIGHT@SNAPS\n" + "=" * 15 + '\n')
+default_choice = 'def'
+#定义寻找模块entity的pattern
+entity_header_pattern = re.compile(key_word_entity + '(.*?)' + key_word_end_entity, re.S)
 
-with open('D:\my_python_trial\myfile1.txt','r') as new_base:
-    new_base_lines = new_base.readlines()
-    new_base_lines[-2]=new_base_lines[-2].replace(')',')end')
-with open('D:\my_python_trial\myfile2.txt','w') as files2:
-   for line in new_base_lines:
-       if key_word_entity not in line:
-         line = re.sub(':','=>',line,1)
-       if key_word_generic in line:
-         line = line.replace(key_word_generic,key_word_generic+'\b'+'map')
-       if 'port' in line:
-         line = line.replace('port','port'+'\b'+'map')
-       if '=>'in line:
-         lable = line.split('=>')[0]
-         instant_signal_name=input("instantiation signal name?")
-         if ')end'in line:
-            line  = lable+'=>'+instant_signal_name+');\n'
-         elif ')'in line:
-            line  = lable+'=>'+instant_signal_name+')\n'
-         else:
-            line  = lable+'=>'+instant_signal_name+',\n'
-       if key_word_end_entity not in line:
-         files2.write(line)
-    # line[2] = line[2].replace(',')234
+#定义函数
+def generate_instance(module_name):
+    files = open('D:\my_python_trial\{}.txt'.format(module_name), "r")  # "r"
+    all_content = files.read()
+    files1 = open('D:\my_python_trial\{}.txt'.format(module_name+'1'),"w")  # "w"
+    instance_name = input('例化名称?')#输入准确模块名称
+    files1.write(header +instance_name+':'+key_word_entity+'\n'.join(entity_header_pattern.findall(all_content)) + key_word_end_entity)#模块中找出entity并加入header
+    files1.close()
 
+    with open('D:\my_python_trial\{}.txt'.format(module_name+'1'),'r') as new_base:
+        new_base_lines = new_base.readlines()
+        new_base_lines[-2]=new_base_lines[-2].replace(')',')end')#找出entity倒数第二行做标记便于后面处理
+    with open('D:\my_python_trial\{}.txt'.format(module_name+'_instance'),'w') as files2:
+       for line in new_base_lines:
+           if key_word_entity not in line:
+             line = re.sub(':','=>',line,1)
+           if key_word_generic in line:
+             line = line.replace(key_word_generic,key_word_generic+'\b'+'map')
+           if 'port' in line:
+             line = line.replace('port','port'+'\b'+'map')
+           if '=>'in line:
+             lable = line.split('=>')[0]
+             lable_stripped = lable.strip()
+             instant_signal_name=input("例化信号名?({})".format(lable_stripped))#输入例化信号名
+             if instant_signal_name == default_choice:#可以选择默认值即被例化的模块信号名
+                instant_signal_name = lable_stripped
+             else:
+                instant_signal_name = instant_signal_name
+             if ')end'in line:
+                line  = lable+'=>'+instant_signal_name+');\n'#之前的倒数第二行，应是被例化后的最后一行，需要加‘：’
+             elif ')'in line:
+                line  = lable+'=>'+instant_signal_name+')\n'
+             else:
+                line  = lable+'=>'+instant_signal_name+',\n'
+           if key_word_end_entity not in line:
+             files2.write(line)
 
-
-# print(try_s)
-# list0 = try_s[1]
-# list1 = try_s[2]
-# list2 = try_s[3]
-# list3 = try_s[4]
-# print(list0)
-# print(list1)
-# print(list2)
-# print(list3)
-# files1.write(header +key_word_entity+'\n'.join(entity_header_pattern.findall(all_content)) + key_word_end_entity)
-
-files.close()
-files1.close()
-files2.close()
-
+    files.close()
+    files1.close()
+    files2.close()
+    return files2
+module_name1 = input('需要例化的模块名：')
+generate_instance(module_name1)
